54bdce93812cc4c3720b41a0b344dd7dce41f3be
yoonjeong <sasohameul@YoonJeongui-MacBookAir.local>
2024-02-15 22:57:20 +0900

    Add modify feat

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const addTask = () => {
     let li = document.createElement('li');
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
-    
+
     let span = document.createElement('span');
     span.innerHTML = "\u00d7";
     li.appendChild(span);
@@ -26,9 +26,55 @@ listContainer.addEventListener('click',function(e){
     e.target.parentElement.remove();
     saveData();
   }
-  
-
 },false);
+
+listContainer.addEventListener('dblclick',function(e){
+
+  if(e.target.tagName === 'LI') {
+    const originText = e.target.textContent.replace('\u00d7', '').trim();
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = originText;
+    e.target.innerHTML = '';
+    e.target.appendChild(input);
+    input.focus();
+
+    input.addEventListener('keypress', function(e){
+      if(e.key === 'Enter' && input.value !== '') {
+        updateTask(this);
+      }
+    });
+    input.addEventListener('blur', function(e){
+      updateTask(this);
+    });
+
+    let isUpdating = false;
+    function updateTask (input)  {
+      if(isUpdating) return;
+      isUpdating = true;
+
+      const newValue = input.value.trim();
+      if(newValue) {
+        const li = input.parentNode;
+        li.innerHTML = newValue;
+        const span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        e.target.appendChild(span);
+      } else {
+        input.parentNode.remove();
+      }
+      saveData();
+      isUpdating = false;
+    }
+  }
+
+});
+
+inputBox.addEventListener('keypress',function(e){
+  if(e.key === 'Enter') {
+    addTask();
+  }
+});
 
 const saveData = () => {
   localStorage.setItem('data', listContainer.innerHTML);
